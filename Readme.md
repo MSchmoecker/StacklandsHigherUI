8bbb35968ed1d09f91f473bc6d40ad1963ffc5d7
Maximilian Schmöcker <m.schmoecker@brickfire.eu>
2022-06-26 15:23:30 +0200

    init

---
+++ Readme.md
@@ -0,0 +1,28 @@
+# Higher UI
+## About
+Increases the height of sidebar elements.
+
+![BepInExSetup](Docs/Showcase.png)
+
+## Config
+Both quests+ideas sidebar and info sidebar height can be set individually.
+The config entries are generated after the first start at `BepInEx/config/com.maxsch.stacklands.higherui.cfg`.
+
+## Installation
+This mod requires BepInEx to work, it is a modding framework that allows multiple mods being loaded.
+Furthermore, this mod uses Harmony to patch into the game, which means no game code is distributed and allows multiple mods to change it interdependent.
+
+1. Download BepInEx from https://github.com/BepInEx/BepInEx/releases 
+2. Extract all files to your Stacklands folder. It should look like this:\
+![BepInExSetup](Docs/BepInExSetup.png)
+3. Download this mod and extract it into `BepInEx/plugins/`
+4. Launch the game! If everything works, you should already see that the height of the sidebar elements is increased.
+
+## Useful Tools
+- ConfigurationManager (https://github.com/BepInEx/BepInEx.ConfigurationManager/releases): Adds the ability to change the config at runtime.
+
+## Development
+1. Install BepInEx
+2. Set your `GAME_PATH` in `StacklandsHigherUI.csproj`
+3. This mod requires publicized game code, this removes the need to get private members via heavy Reflection code. Use https://github.com/CabbageCrow/AssemblyPublicizer for example to publicize `Stacklands/Stacklands_Data/Managed/Assembly-CSharp.dll`
+4. Compile the project. This copies the resulting dll into `<GAME_PATH>/BepInEx/plugins/`
---
+++ StacklandsHigherUI/Mod.cs
@@ -0,0 +1,59 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using UnityEngine;
+
+namespace StacklandsHigherUI {
+    [BepInPlugin(GUID, Name, Version)]
+    [HarmonyPatch]
+    public class Mod : BaseUnityPlugin {
+        public const string Name = "Higher UI";
+        public const string GUID = "com.maxsch.stacklands.higherui";
+        public const string Version = "0.1.0";
+
+        private static Harmony harmony;
+
+        private static ConfigEntry<float> sideBarHeight;
+        private static ConfigEntry<float> infoBoxHeight;
+
+        private void Awake() {
+            harmony = new Harmony(GUID);
+            harmony.PatchAll();
+
+            // 420 is vanilla
+            sideBarHeight = Config.Bind("UI", "Sidebar Height", 620f, "Height of the side bar");
+            sideBarHeight.SettingChanged += (_1, _2) => UpdateSideBarHeight();
+
+            // 200 is vanilla
+            infoBoxHeight = Config.Bind("UI", "Info Box Height", 360f, "Height of the info box");
+            infoBoxHeight.SettingChanged += (_1, _2) => UpdateInfoBoxHeight();
+        }
+
+        [HarmonyPatch(typeof(GameScreen), nameof(GameScreen.Awake)), HarmonyPostfix]
+        private static void AfterGameScreenAwake() {
+            UpdateSideBarHeight();
+            UpdateInfoBoxHeight();
+        }
+
+        private static void UpdateSideBarHeight() {
+            if (!GameScreen.instance) {
+                return;
+            }
+
+            Vector2 sizeDelta = GameScreen.instance.SideTransform.sizeDelta;
+            sizeDelta.y = sideBarHeight.Value;
+            GameScreen.instance.SideTransform.sizeDelta = sizeDelta;
+        }
+
+        private static void UpdateInfoBoxHeight() {
+            if (!GameScreen.instance) {
+                return;
+            }
+
+            RectTransform infoBox = (RectTransform)GameScreen.instance.InfoBoxBackground.transform.parent;
+            Vector2 sizeDelta = infoBox.sizeDelta;
+            sizeDelta.y = infoBoxHeight.Value;
+            infoBox.sizeDelta = sizeDelta;
+        }
+    }
+}
