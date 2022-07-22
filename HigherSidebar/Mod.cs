653f19f3df53d30c4ef0e1569971ecca90b3b1a0
Benedikt Werner <1benediktwerner@gmail.com>
2022-07-22 15:19:05 +0200

    Allow changing debug panel height

--- HigherSidebar/Mod.cs
+++ HigherSidebar/Mod.cs
@@ -14,6 +14,7 @@ namespace HigherSidebar {
         private static Harmony harmony;
 
         private static ConfigEntry<float> infoBoxHeight;
+        private static ConfigEntry<float> debugMenuHeight;
 
         private void Awake() {
             harmony = new Harmony(GUID);
@@ -21,15 +22,20 @@ namespace HigherSidebar {
 
             // 200 is vanilla
             infoBoxHeight = Config.Bind("UI", "Info Box Height", 400f, "Height of the info box. Vanilla is 200");
-            infoBoxHeight.SettingChanged += (_1, _2) => UpdateHeights();
+            infoBoxHeight.SettingChanged += (_1, _2) => UpdateSidebarHeights();
+
+            debugMenuHeight = Config.Bind("UI", "Debug Menu Height", 990f, "Height of the debug menu. Vanilla is 280");
+            debugMenuHeight.SettingChanged += (_1, _2) => UpdateDebugHeight();
         }
 
         [HarmonyPatch(typeof(GameScreen), nameof(GameScreen.Awake)), HarmonyPostfix]
         private static void AfterGameScreenAwake() {
-            UpdateHeights();
+            UpdateSidebarHeights();
+            UpdateDebugHeight();
         }
 
-        private static void UpdateHeights() {
+        private static void UpdateSidebarHeights()
+        {
             if (!GameScreen.instance) {
                 return;
             }
@@ -44,6 +50,18 @@ namespace HigherSidebar {
             sizeDelta = GameScreen.instance.SideTransform.sizeDelta;
             sizeDelta.y = - infoBoxHeight.Value - 15;
             GameScreen.instance.SideTransform.sizeDelta = sizeDelta;
+        }
+
+        private static void UpdateDebugHeight()
+        {
+            if (!GameScreen.instance) {
+                return;
+            }
+
+            RectTransform debugScreen = GameScreen.instance.DebugScreen.rectTransform;
+            Vector2 sizeDelta = debugScreen.sizeDelta;
+            sizeDelta.y = debugMenuHeight.Value;
+            debugScreen.sizeDelta = sizeDelta;
         }
     }
 }
