e50d921e457c8cecee294b42e52df67412a5c269
Maximilian Schmöcker <m.schmoecker@brickfire.eu>
2022-11-19 11:06:35 +0100

    feat: dynamic height

--- HigherSidebar/Mod.cs
+++ HigherSidebar/Mod.cs
@@ -4,6 +4,7 @@ using BepInEx;
 using BepInEx.Configuration;
 using HarmonyLib;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace HigherSidebar {
     [BepInPlugin(GUID, Name, Version)]
@@ -15,15 +16,33 @@ namespace HigherSidebar {
 
         private static Harmony harmony;
 
+        private static ConfigEntry<float> infoBoxMinHeight;
+        private static ConfigEntry<float> infoBoxMaxHeight;
         private static ConfigEntry<float> infoBoxHeight;
+        private static ConfigEntry<bool> dynamicHeight;
+
+        private static float lastHeight;
 
         private void Awake() {
             harmony = new Harmony(GUID);
             harmony.PatchAll();
 
-            // 300 is vanilla
-            string description = $"Height of the info box. Vanilla is 300.{Environment.NewLine}" +
-                                 $"Reloads in-game when the config file is edited or a configuration manager is used.";
+            const string reload = "Reloads in-game when the config file is edited or a configuration manager is used.";
+            string description = "";
+
+            description = $"Dynamically resize the info box based on the preferred text height.{Environment.NewLine}{reload}";
+            dynamicHeight = Config.Bind("Dynamic UI", "Use Dynamic Height", true, description);
+            dynamicHeight.SettingChanged += (_1, _2) => UpdateSidebarHeights();
+
+            description = $"Min height of the info box. Used if Dynamic Height is active.{Environment.NewLine}{reload}";
+            infoBoxMinHeight = Config.Bind("Dynamic UI", "Info Box Min Height", 300f, description);
+            infoBoxMinHeight.SettingChanged += (_1, _2) => UpdateSidebarHeights();
+
+            description = $"Max height of the info box. Used if Dynamic Height is active.{Environment.NewLine}{reload}";
+            infoBoxMaxHeight = Config.Bind("Dynamic UI", "Info Box Max Height", 700f, description);
+            infoBoxMaxHeight.SettingChanged += (_1, _2) => UpdateSidebarHeights();
+
+            description = $"Height of the info box. Used if Dynamic Height is not active. Vanilla is 300.{Environment.NewLine}{reload}";
             infoBoxHeight = Config.Bind("UI", "Info Box Height", 400f, description);
             infoBoxHeight.SettingChanged += (_1, _2) => UpdateSidebarHeights();
 
@@ -40,9 +59,19 @@ namespace HigherSidebar {
             watcher.SynchronizingObject = ThreadingHelper.SynchronizingObject;
         }
 
-        [HarmonyPatch(typeof(GameScreen), nameof(GameScreen.Awake)), HarmonyPostfix]
-        private static void AfterGameScreenAwake() {
+        [HarmonyPatch(typeof(GameScreen), nameof(GameScreen.LateUpdate)), HarmonyPostfix]
+        private static void AfterGameScreenUpdate() {
             UpdateSidebarHeights();
+        }
+
+        private static float CalculateHeight() {
+            if (!dynamicHeight.Value) {
+                return infoBoxHeight.Value;
+            }
+
+            GameScreen.instance.InfoText.text = GameScreen.instance.InfoText.text.Trim();
+            float preferredHeight = GameScreen.instance.Valuetext.preferredHeight + GameScreen.instance.InfoText.preferredHeight + 70f;
+            return Mathf.Clamp(preferredHeight, infoBoxMinHeight.Value, infoBoxMaxHeight.Value);
         }
 
         private static void UpdateSidebarHeights() {
@@ -50,16 +79,26 @@ namespace HigherSidebar {
                 return;
             }
 
+            float newHeight = CalculateHeight();
+
+            if (Math.Abs(lastHeight - newHeight) < 1f) {
+                return;
+            }
+
+            lastHeight = newHeight;
+
             // InfoBox
-            RectTransform infoBox = (RectTransform)GameScreen.instance.ValueParent.transform.parent;
+            RectTransform infoBox = (RectTransform)GameScreen.instance.InfoBox.transform;
             Vector2 sizeDelta = infoBox.sizeDelta;
-            sizeDelta.y = infoBoxHeight.Value;
+            sizeDelta.y = newHeight;
             infoBox.sizeDelta = sizeDelta;
 
             // SideBar
             sizeDelta = GameScreen.instance.SideTransform.sizeDelta;
-            sizeDelta.y = - infoBoxHeight.Value - 15;
+            sizeDelta.y = -newHeight - 15;
             GameScreen.instance.SideTransform.sizeDelta = sizeDelta;
+
+            LayoutRebuilder.ForceRebuildLayoutImmediate(GameScreen.instance.SideTransform);
         }
     }
 }
