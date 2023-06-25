29cbe5344d3c2005ec2391217b4ef3b4b2fb3aa3
Maximilian Schmöcker <m.schmoecker@brickfire.eu>
2023-06-25 17:19:26 +0200

    chore: update to beta mod loader

--- HigherSidebar/HigherSidebarMod.cs
+++ HigherSidebar/HigherSidebarMod.cs
@@ -1,32 +1,28 @@
 ﻿using System;
-using System.IO;
-using BepInEx;
-using BepInEx.Configuration;
 using HarmonyLib;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace HigherSidebar {
-    [BepInPlugin(GUID, Name, Version)]
     [HarmonyPatch]
-    public class Mod : BaseUnityPlugin {
+    public class HigherSidebarMod : Mod {
         public const string Name = "Higher Sidebar";
         public const string GUID = "com.maxsch.stacklands.higherui";
-        public const string Version = "0.3.0";
+        public const string Version = "0.4.0";
 
         private static Harmony harmony;
 
-        private static ConfigEntry<float> infoBoxMinHeight;
-        private static ConfigEntry<float> infoBoxMaxHeight;
-        private static ConfigEntry<float> infoBoxHeight;
-        private static ConfigEntry<bool> dynamicHeight;
-        private static ConfigEntry<float> heightChangeCooldown;
+        private static ConfigEntry infoBoxMinHeight;
+        private static ConfigEntry infoBoxMaxHeight;
+        private static ConfigEntry infoBoxHeight;
+        private static ConfigEntry dynamicHeight;
+        private static ConfigEntry heightChangeCooldown;
 
         private static float lastHeight;
         private static float heightCooldown;
 
-        private void Awake() {
+        public override void Ready() {
             harmony = new Harmony(GUID);
             harmony.PatchAll();
 
@@ -34,36 +30,29 @@ namespace HigherSidebar {
             string description = "";
 
             description = $"Dynamically resize the info box based on the preferred text height.{Environment.NewLine}{reload}";
-            dynamicHeight = Config.Bind("Dynamic UI", "Use Dynamic Height", true, description);
-            dynamicHeight.SettingChanged += (_1, _2) => UpdateSidebarHeights();
+            dynamicHeight = Config.GetEntry<bool>("Use Dynamic Height", true);
+            dynamicHeight.ExtraData["tooltip"] = description;
+            // dynamicHeight.SettingChanged += (_1, _2) => UpdateSidebarHeights();
 
             description = $"Min height of the info box. Used if Dynamic Height is active.{Environment.NewLine}{reload}";
-            infoBoxMinHeight = Config.Bind("Dynamic UI", "Info Box Min Height", 300f, description);
-            infoBoxMinHeight.SettingChanged += (_1, _2) => UpdateSidebarHeights();
+            infoBoxMinHeight = Config.GetEntry<float>("Info Box Min Height", 300f);
+            infoBoxMinHeight.ExtraData["tooltip"] = description;
+            // infoBoxMinHeight.SettingChanged += (_1, _2) => UpdateSidebarHeights();
 
             description = $"Max height of the info box. Used if Dynamic Height is active.{Environment.NewLine}{reload}";
-            infoBoxMaxHeight = Config.Bind("Dynamic UI", "Info Box Max Height", 700f, description);
-            infoBoxMaxHeight.SettingChanged += (_1, _2) => UpdateSidebarHeights();
+            infoBoxMaxHeight = Config.GetEntry<float>( "Info Box Max Height", 700f);
+            infoBoxMaxHeight.ExtraData["tooltip"] = description;
+            // infoBoxMaxHeight.SettingChanged += (_1, _2) => UpdateSidebarHeights();
 
             description = $"Height of the info box. Used if Dynamic Height is not active. Vanilla is 300.{Environment.NewLine}{reload}";
-            infoBoxHeight = Config.Bind("UI", "Info Box Height", 400f, description);
-            infoBoxHeight.SettingChanged += (_1, _2) => UpdateSidebarHeights();
+            infoBoxHeight = Config.GetEntry<float>("Info Box Height", 400f);
+            infoBoxHeight.ExtraData["tooltip"] = description;
+            // infoBoxHeight.SettingChanged += (_1, _2) => UpdateSidebarHeights();
 
             description = $"Time before the dynamic height is shrunken again. Helps to box resizes when moving between cards.{Environment.NewLine}{reload}";
-            heightChangeCooldown = Config.Bind("Dynamic UI", "Height Change Cooldown", 0.1f, description);
-            heightChangeCooldown.SettingChanged += (_1, _2) => UpdateSidebarHeights();
-
-            CreateConfigWatcher();
-        }
-
-        private void CreateConfigWatcher() {
-            FileSystemWatcher watcher = new FileSystemWatcher(Paths.ConfigPath, Path.GetFileName(Config.ConfigFilePath));
-            watcher.Changed += (_1, _2) => Config.Reload();
-            watcher.Created += (_1, _2) => Config.Reload();
-            watcher.Renamed += (_1, _2) => Config.Reload();
-            watcher.EnableRaisingEvents = true;
-            watcher.IncludeSubdirectories = true;
-            watcher.SynchronizingObject = ThreadingHelper.SynchronizingObject;
+            heightChangeCooldown = Config.GetEntry<float>("Height Change Cooldown", 0.1f);
+            heightChangeCooldown.ExtraData["tooltip"] = description;
+            // heightChangeCooldown.SettingChanged += (_1, _2) => UpdateSidebarHeights();
         }
 
         [HarmonyPatch(typeof(GameScreen), nameof(GameScreen.LateUpdate)), HarmonyPostfix]
@@ -83,13 +72,13 @@ namespace HigherSidebar {
         }
 
         private static float CalculateHeight() {
-            if (!dynamicHeight.Value) {
-                return infoBoxHeight.Value;
+            if (!dynamicHeight.GetBool()) {
+                return infoBoxHeight.GetFloat();
             }
 
             GameScreen.instance.InfoText.text = GameScreen.instance.InfoText.text.Trim();
             float preferredHeight = GameScreen.instance.Valuetext.preferredHeight + GameScreen.instance.InfoText.preferredHeight + 72f;
-            return Mathf.Clamp(preferredHeight, infoBoxMinHeight.Value, infoBoxMaxHeight.Value);
+            return Mathf.Clamp(preferredHeight, infoBoxMinHeight.GetFloat(), infoBoxMaxHeight.GetFloat());
         }
 
         private static void UpdateSidebarHeights() {
@@ -104,7 +93,7 @@ namespace HigherSidebar {
                 return;
             }
 
-            if (newHeight < lastHeight && heightCooldown < heightChangeCooldown.Value) {
+            if (newHeight < lastHeight && heightCooldown < heightChangeCooldown.GetFloat()) {
                 heightCooldown += Time.deltaTime;
                 return;
             }
--- HigherSidebar/Properties/AssemblyInfo.cs
+++ HigherSidebar/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using HigherSidebar;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion(Mod.Version)]
-[assembly: AssemblyFileVersion(Mod.Version)]
+[assembly: AssemblyVersion(HigherSidebarMod.Version)]
+[assembly: AssemblyFileVersion(HigherSidebarMod.Version)]
